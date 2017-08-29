3e5afa24250a0fbebf27d4fc81fd3cfa9da4a988
Fabricio Bizotto <fabricio.bizotto@gmail.com>
2017-08-29 11:09:44 -0300

    produtos

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,7 @@ class Product < ApplicationRecord
   include Filterable
   mount_uploader :image, ProductUploader
 
+  validates :name, :description, presence:true
+
   scope :name_contains, -> (name) { where("name like ?", "%#{name}%")}
 end
