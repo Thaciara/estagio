cd4db71f37b8a2ba39fc0d5683bd91b56173e0e7
Thaciara <taciara0105@gmail.com>
2017-08-30 14:58:44 -0300

    modificações feitas em 30/08

--- db/schema.rb
+++ db/schema.rb
@@ -15,13 +15,6 @@ ActiveRecord::Schema.define(version: 20170829221837) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "clientes", force: :cascade do |t|
-    t.string "nome", limit: 100, null: false
-    t.string "logo", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "products", force: :cascade do |t|
     t.string "name", null: false
     t.text "description", null: false
