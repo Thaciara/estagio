222c8928a6548cc8d9a6cebf6f47eacd5b72a870
Thaciara <taciara0105@gmail.com>
2017-09-02 19:58:35 -0300

    modificações feitas em 02/09

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+	protected
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.for(:account_update) << :name
+		devise_parameter_sanitizer.for(:sign_up) << :name
+	end
+
   protect_from_forgery with: :exception
 
   # Página inicial
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :admin_users
   resources :clients
   resources :products
   resources :clientes
