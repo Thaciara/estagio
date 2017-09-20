c484cf2c68fb4f8688cf84be02330bd0e9ba9455
Thaciara <taciara0105@gmail.com>
2017-09-20 14:32:09 -0300

    modificações feitas em 20/09

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,6 +2,7 @@ class VisitorsController < ApplicationController
 
   def index
     @produtos = Product.where(idprincipal:true, active:true)
+    @clientes = Client.all
     # @produtos.each_slice(3).to_a
   end
 
