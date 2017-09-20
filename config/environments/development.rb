310bdc8397c81747f6f10d91713517142db3386b
Fabricio Bizotto <fabricio.bizotto@gmail.com>
2017-09-20 18:58:42 -0300

    commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,4 +53,17 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD']
+  }
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -50,14 +50,6 @@ ActiveRecord::Schema.define(version: 20170913164446) do
     t.boolean "idprincipal", default: false
   end
 
-  create_table "produtos", force: :cascade do |t|
-    t.string "nome"
-    t.text "descrição"
-    t.string "imagem"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
